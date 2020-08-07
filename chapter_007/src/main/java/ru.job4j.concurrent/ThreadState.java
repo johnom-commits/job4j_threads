678f1333a364706024e0f16eaf018222787631ac
johnom-commits <johnom2012@yandex.ru>
2020-08-07 20:26:29 +0300

    Состояние нити

---
+++ chapter_007/src/main/java/ru.job4j.concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread mainThread = Thread.currentThread();
+        Thread first = new Thread(() -> {}, "First thread");
+        System.out.println(first.getName() + ": " + first.getState());
+        first.start();
+
+        Thread second = new Thread(() -> {}, "Second thread");
+        System.out.println(second.getName()  + ": " + second.getState());
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            printState(first, second);
+        }
+        printState(first, second);
+        while (second.getState() != Thread.State.TERMINATED) {
+            mainThread.sleep(1);
+        }
+        System.out.println("Работа завершена. Все свободны");
+    }
+
+    private static void printState(Thread first, Thread second) {
+        System.out.println(first.getName() + ": " + first.getState());
+        System.out.println(second.getName() + ": " + second.getState());
+    }
+}
