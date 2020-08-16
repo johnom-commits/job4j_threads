aa3b33393c3bc29457618afa15197e45a1958491
johnom-commits <johnom2012@yandex.ru>
2020-08-16 12:31:16 +0300

    Class UserStorage v2

--- chapter_007/src/main/java/ru/job4j/net/UserStorage.java
+++ chapter_007/src/main/java/ru/job4j/net/UserStorage.java
@@ -3,20 +3,24 @@ package ru.job4j.net;
 import net.jcip.annotations.ThreadSafe;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @ThreadSafe
 public final class UserStorage {
     private final Map<Integer, User> map = new HashMap<>();
 
     public synchronized boolean add(User user) {
+        Objects.requireNonNull(user);
         return map.put(user.getId(), user) != null;
     }
 
     public synchronized boolean update(int id, User user) {
+        Objects.requireNonNull(user);
         return map.replace(id, map.get(id), user);
     }
 
     public synchronized boolean delete(User user) {
+        Objects.requireNonNull(user);
         return map.remove(user.getId()) != null;
     }
 
@@ -24,12 +28,15 @@ public final class UserStorage {
         return map.size();
     }
 
-    public synchronized void transfer(int fromId, int toId, int amount) {
+    public synchronized void transfer(int fromId, int toId, int amount) throws Exception {
         User userFrom = map.get(fromId);
-        User userTo = map.get(toId);
         int amountFrom = userFrom.getAmount();
-        userFrom.setAmount(amountFrom -= amount);
+        if (amountFrom < amount) {
+            throw new Exception("Недостаточно суммы на счёте!");
+        }
+        User userTo = map.get(toId);
         int amountTo = userTo.getAmount();
+        userFrom.setAmount(amountFrom -= amount);
         userTo.setAmount(amountTo += amount);
     }
 }
